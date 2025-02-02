01fe6e5ff7b800d2f63ea7c82b8d439121f33ebd
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-02 17:26:08 -0500

    💄 update Layout component to conditionally render Navbar and Footer based on authentication page

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import Head from 'next/head'
-
-import Navbar from './Navbar'
-import Footer from './Footer'
+'use client'
+import React from "react";
+import Head from "next/head";
+import { usePathname } from "next/navigation";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Layout({ children }) {
+    const pathname = usePathname();
+    const isAuthPage = pathname?.startsWith("/auth");
+
     return (
         <div>
-            <header>
-                <Navbar />
-            </header>
+            {!isAuthPage && (
+                <header>
+                    <Navbar />
+                </header>
+            )}
             <main>{children}</main>
-            <Footer/>
+            {!isAuthPage && <Footer />}
         </div>
     );
 }
 
-export default Layout
+export default Layout;
