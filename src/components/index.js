7c104ba2ee514469b505d7b407f7648b37296524
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-02 16:24:44 -0500

    💄 add ProductList export to components index

--- src/components/index.js
+++ src/components/index.js
@@ -5,3 +5,4 @@ export { default as Cart } from './Cart.jsx';
 export { default as HeroBanner } from './HeroBanner.jsx';
 export { default as Navbar } from './Navbar.jsx';
 export { default as Product } from './Product.jsx';
+export { default as ProductList } from './ProductList.jsx';
