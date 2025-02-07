9239b27147b64a7aeb7a9590f3cd63bbb0ea2be5
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-07 09:02:05 -0500

    💄 refactor Layout component for consistent formatting and conditional class application

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,25 +1,25 @@
-'use client'
-import React from "react";
-import Head from "next/head";
-import { usePathname } from "next/navigation";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+'use client';
+import React from 'react';
+import Head from 'next/head';
+import { usePathname } from 'next/navigation';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
 function Layout({ children }) {
-    const pathname = usePathname();
-    const isAuthPage = pathname?.startsWith("/auth");
+	const pathname = usePathname();
+	const isAuthPage = pathname?.startsWith('/auth');
 
-    return (
-        <div>
-            {!isAuthPage && (
-                <header>
-                    <Navbar />
-                </header>
-            )}
-            <main className="pt-16">{children}</main>
-            {!isAuthPage && <Footer />}
-        </div>
-    );
+	return (
+		<div>
+			{!isAuthPage && (
+				<header>
+					<Navbar />
+				</header>
+			)}
+			<main className={!isAuthPage ? 'pt-16' : ''}>{children}</main>
+			{!isAuthPage && <Footer />}
+		</div>
+	);
 }
 
 export default Layout;
